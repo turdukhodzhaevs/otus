c5fa37e9ebf0af61c4940fc109fae68251bd8f8a
aidar61 <aaskeev@colvir.com>
2023-09-14 11:16:24 +0600

    Added test

---
+++ Homework/QuadraticEquationService.cs
@@ -0,0 +1,38 @@
+﻿namespace Services;
+
+public static class QuadraticEquationService
+{
+    public static double[] Solve(double a, double b, double c)
+    {
+        if (double.IsNaN(a) || double.IsNaN(b) || double.IsNaN(c))
+        {
+            throw new ArgumentException("Аргумент не является числом");
+        }
+
+        if (double.IsInfinity(a) || double.IsInfinity(b) || double.IsInfinity(c))
+        {
+            throw new ArgumentException("Аргумент является бесконечностью");
+        }
+
+        if (Math.Abs(a) < double.Epsilon)
+        {
+            throw new ArgumentException("Коэффициент a не может быть равен 0", "a");
+        }
+
+        var discriminant = b * b - 4 * a * c;
+
+        if (discriminant < 0)
+        {
+            return Array.Empty<double>(); // корней нет
+        }
+
+        if (Math.Abs(discriminant) < double.Epsilon)
+        {
+            return new[] { -b / (2 * a) }; // один корень
+        }
+
+        var x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+        var x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+        return new[] { x1, x2 }; // два корня
+    }
+}
---
+++ Homework/QuadraticEquationTest.cs
@@ -0,0 +1,59 @@
+using Services;
+
+namespace QuadraticEquation;
+
+public class QuadraticEquationTest
+{
+    [Fact]
+       public void TestNoRoots()
+       {
+           var a = 1.0;
+           var b = 0.0;
+           var c = 1.0;
+           var expected = Array.Empty<double>();
+           var actual = QuadraticEquationService.Solve(a, b, c);
+           Assert.Equal(expected, actual);
+       }
+   
+       [Fact]
+       public void TestTwoRoots()
+       {
+           double[] expected = { 1, -1 };
+           double[] actual = QuadraticEquationService.Solve(1, 0, -1);
+           Assert.Equal(expected, actual);
+       }
+   
+       [Fact]
+       public void TestOneRoot()
+       {
+           var expected = new double[] { -1 };
+           var actual = QuadraticEquationService.Solve(1, 2, 1);
+           Assert.Equal(expected, actual);
+       }
+   
+       [Fact]
+       public void TestAEqualsZero()
+       {
+           var a = 0.0;
+           var b = 2.0;
+           var c = 3.0;
+           
+           Assert.Throws<ArgumentException>(() => QuadraticEquationService.Solve(a, b, c));
+       }
+   
+       [Fact]
+       public void TestSolveWithInvalidCoefficients()
+       {
+           double[] invalidCoefficients = { double.NaN, double.NegativeInfinity, double.PositiveInfinity };
+           foreach (var a in invalidCoefficients)
+           {
+               foreach (var b in invalidCoefficients)
+               {
+                   foreach (var c in invalidCoefficients)
+                   {
+                       Assert.Throws<ArgumentException>(() => QuadraticEquationService.Solve(a, b, c));
+                   }
+               }
+           }
+       }
+}
